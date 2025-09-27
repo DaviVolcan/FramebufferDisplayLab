3fdafa55a1166a85990bf85661df6d55b63f56ba
davivolcan <davi.volcan2@gmail.com>
2025-09-27 12:44:26 -0300

    feat: initialize framebuffer and set a pixel value

--- main.c
+++ main.c
@@ -1,7 +1,35 @@
+#include <fcntl.h>
+#include <linux/fb.h>
 #include <stdio.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
-int main(void)
-{
-    printf("Hello, World!\n");
+void set_pixel_value(struct fb_var_screeninfo info, char* tela, int x, int y){
+    const unsigned int bytes_per_line = info.xres / 8;
+    const unsigned int mem_pos = y * bytes_per_line + x * info.bits_per_pixel / 8;
+    const char value = 0x01 << x % 8;
+    printf("mem_pos = %d, value = %d (0x%02X)\n", mem_pos, value,
+           (unsigned char)value);
+    tela[mem_pos] |= value;
+}
+
+int main() {
+    int fb = open("/dev/fb0", O_RDWR);
+
+    struct fb_var_screeninfo info;
+    ioctl(fb, FBIOGET_VSCREENINFO, &info);
+
+    printf("Tela: %dx%d\n", info.xres, info.yres);
+
+    int tamanho = info.xres * info.yres / 8;
+    char* screen = (char*)mmap(0, tamanho, PROT_WRITE, MAP_SHARED, fb, 0);
+
+    int x = 0;
+    int y = 0;
+
+    set_pixel_value(info, screen, x, y);
+
+    close(fb);
     return 0;
 }
