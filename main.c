240a5b363de228e2bf21e906dcb56f1ad1d14704
davivolcan <davi.volcan2@gmail.com>
2025-09-27 22:07:18 -0300

    refactor: introduce `framebuffer_context` struct and update related functions for clarity and maintainability

--- main.c
+++ main.c
@@ -5,6 +5,14 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include "font8x8_basic.h"
+
+struct framebuffer_context
+{
+    struct fb_var_screeninfo info;
+    char* screen;
+};
 
 struct coordinates
 {
@@ -18,27 +26,29 @@ int validate_coordinates(const struct fb_var_screeninfo info, const struct coord
     return 0;
 }
 
-static void set_pixel_in_framebuffer(const struct fb_var_screeninfo info, char* screen, const struct coordinates point)
+static void set_pixel_in_framebuffer(const struct framebuffer_context* fb, const struct coordinates point)
 {
     const unsigned int bytes_per_line = info.xres / 8;
     const unsigned int mem_pos = point.y * bytes_per_line + point.x * info.bits_per_pixel / 8;
+    const unsigned int bytes_per_line = fb->info.xres / 8;
+    const unsigned int mem_pos = point.y * bytes_per_line + point.x * fb->info.bits_per_pixel / 8;
     const char value = 0x01 << point.x % 8;
-    screen[mem_pos] |= value;
+    fb->screen[mem_pos] |= value;
 }
 
-int set_pixel_value(const struct fb_var_screeninfo info, char* tela, const struct coordinates point)
+int set_pixel_value(const struct framebuffer_context* fb, const struct coordinates point)
 {
-    if (validate_coordinates(info, point) != 0) return -1;
-    set_pixel_in_framebuffer(info, tela, point);
+    if (validate_coordinates(fb->info, point) != 0) return -1;
+    set_pixel_in_framebuffer(fb, point);
 
     return 0;
 }
 
 
-int draw_line(struct fb_var_screeninfo info, char* screen, const struct coordinates start, const struct coordinates end)
+int draw_line(const struct framebuffer_context* fb, const struct coordinates start, const struct coordinates end)
 {
     //breBresenham's line algorithm
-    if (validate_coordinates(info, start) != 0 || validate_coordinates(info, end) != 0) return -1;
+    if (validate_coordinates(fb->info, start) != 0 || validate_coordinates(fb->info, end) != 0) return -1;
 
     int dx = abs((int)end.x - (int)start.x);
     int dy = abs((int)end.y - (int)start.y);
@@ -51,7 +61,7 @@ int draw_line(struct fb_var_screeninfo info, char* screen, const struct coordina
 
     while (1)
     {
-        set_pixel_in_framebuffer(info, screen, current_point);
+        set_pixel_in_framebuffer(fb, current_point);
 
         if (current_point.x == end.x && current_point.y == end.y) break;
 
@@ -71,35 +81,48 @@ int draw_line(struct fb_var_screeninfo info, char* screen, const struct coordina
     return 0;
 }
 
+int is_bit_set(char c, unsigned int bit)
+{
+    return (c >> bit) & 1;
+}
+
+
+void draw_character(const struct framebuffer_context* fb, const struct coordinates offset,
+                    unsigned int character_number)
+{
+    struct coordinates bit_cordinates = {0, 0};
+    for (unsigned int y = 0; y < 8; y++)
+    {
+        bit_cordinates.y = offset.y + y;
+        for (unsigned int x = 0; x < 8; x++)
+        {
+            bit_cordinates.x = offset.x + x;
+            if (is_bit_set(font8x8_basic[character_number][y], x))
+            {
+                set_pixel_in_framebuffer(fb, bit_cordinates);
+            }
+        }
+    }
+}
+
 int main()
 {
     int fb = open("/dev/fb0", O_RDWR);
+    struct framebuffer_context fb_ctx;
+    ioctl(fb, FBIOGET_VSCREENINFO, &fb_ctx.info);
 
-    struct fb_var_screeninfo info;
-    ioctl(fb, FBIOGET_VSCREENINFO, &info);
+    printf("Screen: %dx%d\n", fb_ctx.info.xres, fb_ctx.info.yres);
 
-    printf("Tela: %dx%d\n", info.xres, info.yres);
-
-    unsigned int tamanho = info.xres * info.yres / 8;
-    char* screen = (char*)mmap(0, tamanho, PROT_WRITE, MAP_SHARED, fb, 0);
+    unsigned int tamanho = fb_ctx.info.xres * fb_ctx.info.yres / 8;
+    fb_ctx.screen = (char*)mmap(0, tamanho, PROT_WRITE, MAP_SHARED, fb, 0);
 
     struct coordinates point_a = {10, 10};
     struct coordinates point_b = {118, 54};
 
     struct coordinates invalid_point = {150, 50};
 
-    set_pixel_value(info, screen, invalid_point);
-    if (draw_line(info, screen, point_a, point_b) != 0)
-    {
-        printf("Error: coordinate (%u, %u) is invalid, could not set pixel.\n",
-               invalid_point.x, invalid_point.y);
-    }
-    else
-    {
-        printf("Pixel set successfully!\n");
-    }
 
-
+    draw_character(&fb_ctx, point_a, 0x29);
     close(fb);
     return 0;
 }
